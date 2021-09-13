360f70362b6f9cbed8b5308e8d848f6acf83f8a5
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-13 22:59:30 +0530

    Added: Welcome Message

---
+++ src/com/bridgelabz/LineComparision.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class LineComparision {
+
+    public void welcome(){
+        System.out.println("......Welcome to Line Comparison Computation Program using OOPS...");
+    }
+
+
+    public static void main(String[] args) {
+        LineComparision line = new LineComparision();
+        line.welcome();
+
+    }
+}
